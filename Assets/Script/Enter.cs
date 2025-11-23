2353b6325422e8a136cc8d197f5754917f8091bb
xupeng <xupeng@gamespark.net>
2025-11-23 13:38:54 +0800

    漫画浏览页实现

--- Assets/Script/Enter.cs
+++ Assets/Script/Enter.cs
@@ -24,8 +24,13 @@ public class Enter : MonoBehaviour
         var nodeData = MangaContainer.Instance.GetNodeDataByID(nodeId);
         MangaContainer.Instance.CurrNodeData = nodeData;
         Debug.Log("OnEnterBtnClick");
-        ResourcesManager.Instance.LoadGameObject("Prefab/UI_MangaRender", (GameObject asset) =>
+        ResourcesManager.Instance.LoadGameObject("Prefab/UI_Manga", (GameObject asset) =>
         {
+            if (asset == null)
+            {
+                Debug.LogError("UI_Manga asset is null");
+                return;
+            }
             var go = GameObject.Instantiate(asset);
             ResourcesManager.Instance.SetParent(go, parent);
         });
